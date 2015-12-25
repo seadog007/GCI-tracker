c0bab2edb00674ed362b65d8bbd63ce0382b4838
Jasper Yu <007seadog@gmail.com>
2015-12-26 01:42:54 +0800

    Beginner and exhausted tasks

--- tracker_html.sh
+++ tracker_html.sh
@@ -3,28 +3,40 @@
 # If you are using Debian, Ubuntu, or something, then you can use `sudo apt-get install jq`
 # I don't use yum and rpm system, so XDDDD Google it!
 # Oh, Windows user? Sorry, you need a unix system.
+
+orgid='4625502878826496' #FOSSASIA
+#orgid='4568116747042816' #Ubuntu
+
 echo '<html>
 		<head>
+			<meta name="viewport" content="width=device-width, initial-scale=1">
 			<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
 		</head>
 		<body>
-			<table class="table">
-				<thead>
-					<td width="58%">Task Name</td>
-					<td width="7%">Approved</td>
-					<td width="7%">Total Working</td>
-					<td width="7%">Failed</td>
-					<td width="7%">Available instances</td>
-					<td width="7%">Total claimed</td>
-					<td width="7%">Max instances</td>
-				</thead>
-			<tbody>'
+		<div class="container" style="padding-top: 20px;">
+			<div class="table-responsive">
+				<table class="table table-bordered table-striped">
+					<thead>
+						<tr>
+							<td width="58%">Task Name</td>
+							<td width="6%">Time Limit</td>
+							<td width="6%">Approved</td>
+							<td width="6%">Total Working</td>
+							<td width="6%">Failed</td>
+							<td width="6%">Available instances</td>
+							<td width="6%">Total claimed</td>
+							<td width="6%">Max instances</td>
+						</tr>
+					</thead>
+				<tbody>'
 while read -r res
 do
 
 	# Base data
 	name=`echo "$res" | jq -r .name`
 	time_to_complete_in_days=`echo "$res" | jq -r .time_to_complete_in_days`
+	is_beginner=`echo "$res" | jq -r .is_beginner`
+	is_exhausted=`echo "$res" | jq -r .is_exhausted`
 
 	# Stat data
 	completed_count=`echo "$res" | jq -r .completed_count`
@@ -40,10 +52,20 @@ do
 	available_count=`echo "$res" | jq -r .available_count`
 	max_instances=`echo "$res" | jq -r .max_instances`
 
-	echo '<tr>'
+	if [ "$is_exhausted" == "false" ]
+	then
+		if [ "$is_beginner" == "false" ]
+		then
+			echo '<tr>'
+		else
+			echo '<tr class="success">'
+		fi
+	else
+		echo '<tr class="error">'
+	fi
 	echo -n "<td>$name</td>"
-#	[ "$time_to_complete_in_days" -gt 1 ] && echo "Time limit: $time_to_complete_in_days days"
-#	[ "$time_to_complete_in_days" -eq 1 ] && echo "Time limit: $time_to_complete_in_days day"
+	[ "$time_to_complete_in_days" -gt 1 ] && echo -n "<td>$time_to_complete_in_days days</td>"
+	[ "$time_to_complete_in_days" -eq 1 ] && echo -n "<td>$time_to_complete_in_days day</td>"
 	echo -n "<td>$completed_count</td>"
 	echo -n "<td>$active_count</td>"
 #	echo "　├─Student not finish: $in_progress_count"
@@ -56,7 +78,13 @@ do
 	echo -n "<td>$claimed_count</td>"
 	echo -n "<td>$max_instances</td>"
 	echo '</tr>'
-done < <(curl -s 'https://codein.withgoogle.com/api/program/2015/taskdefinition/?is_beginner=False&is_exhausted=False&organization=4625502878826496&page=1&page_size=100&status=-1' | jq -c '.results[]')
-echo '</tbody>
-	</body>
+#done < <(curl -s "https://codein.withgoogle.com/api/program/2015/taskdefinition/?is_beginner=False&is_exhausted=False&organization=$orgid&page=1&page_size=1000&status=0" | jq -c '.results[]')
+#done < <(curl -s "https://codein.withgoogle.com/api/program/2015/taskdefinition/?is_exhausted=False&organization=$orgid&page=1&page_size=1000&status=0" | jq -c '.results[]')
+#done < <(curl -s "https://codein.withgoogle.com/api/program/2015/taskdefinition/?is_beginner=False&organization=$orgid&page=1&page_size=1000&status=0" | jq -c '.results[]')
+done < <(curl -s "https://codein.withgoogle.com/api/program/2015/taskdefinition/?organization=$orgid&page=1&page_size=1000&status=0" | jq -c '.results[]')
+echo '					</tbody>
+					</table>
+				</div>
+			</div>
+		</body>
 	</html>'
